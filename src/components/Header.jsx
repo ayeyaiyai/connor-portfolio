895c98ccd08339bb588c29d14df72b7239298676
ayeyaiyai <105176518+ayeyaiyai@users.noreply.github.com>
2023-11-11 17:58:17 -0500

    Add Header elements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,16 @@
-import '../styles/Header.css'
+import '../styles/Header.css';
 
-function Header() {
+function Header() {    
     return (
         <section className='header-container'>
-
+            <div className='header-body'>
+                <div className='header-name header-link'>Connor Gilbert</div>
+                <div className='header-links'>
+                    <span className='header-link'>Work</span>
+                    <span className='header-link'>About Me</span>
+                    <span className='header-link'>For Fun</span>
+                </div>
+            </div>
         </section>
     )
 }
