533144f0e15a97851d8b550f3ac272bd99122cf0
ayeyaiyai <105176518+ayeyaiyai@users.noreply.github.com>
2023-11-12 15:22:36 -0500

    Add link functionality to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/Header.css';
 
-function Header() {    
-    return (
-        <section className='header-container'>
-            <div className='header-body'>
-                <div className='header-name header-link'>Connor Gilbert</div>
-                <div className='header-links'>
-                    <span className='header-link'>Work</span>
-                    <span className='header-link'>About Me</span>
-                    <span className='header-link'>For Fun</span>
-                </div>
-            </div>
-        </section>
-    )
+function Header() {
+  return (
+    <section className='header-container'>
+      <div className='header-body'>
+        <Link to='/' className='header-name header-link'>
+          Connor Gilbert
+        </Link>
+        <div className='header-links'>
+          <Link to='/work' className='header-link'>
+            Work
+          </Link>
+          <Link to='/about' className='header-link'>
+            About Me
+          </Link>
+          <Link to='/for-fun' className='header-link'>
+            For Fun
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
 }
+
 export default Header;
