8553b0e561f779dd6215d8ec1f51859a3833e405
ayeyaiyai <105176518+ayeyaiyai@users.noreply.github.com>
2023-11-12 15:22:14 -0500

    Add Link functionality to header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Header from './components/Header';
 import Home from './components/Home';
+import Works from './components/Works'; 
+import About from './components/About';
+import ForFun from './components/ForFun';
 
 function App() {
- 
   return (
-    <div className='App'>
-      <Home />
-    </div>
+    <Router>
+      <div className='App'>
+        <Routes>
+          <Route path='/work' element={<Works />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/for-fun' element={<ForFun />} />
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
