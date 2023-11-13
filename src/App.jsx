663cc9d9daaca0e7e884348790673c02a216e9d6
ayeyaiyai <105176518+ayeyaiyai@users.noreply.github.com>
2023-11-13 11:57:42 -0500

    Import ScrollToTop

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './components/Home';
 import Works from './components/Works'; 
 import About from './components/About';
@@ -9,6 +10,7 @@ import ForFun from './components/ForFun';
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className='App'>
         <Routes>
           <Route path='/work' element={<Works />} />
