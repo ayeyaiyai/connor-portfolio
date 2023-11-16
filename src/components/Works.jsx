ae739e8bbdf23e040f64ba33b3591fde4bbdc77e
ayeyaiyai <105176518+ayeyaiyai@users.noreply.github.com>
2023-11-16 16:51:30 -0500

    Add link to WhereInWorld section

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Header from "./Header";
 import Footer from './Footer';
 import rightArrow from '../images/arrow-forward-outline.svg';
@@ -14,22 +15,25 @@ function Works() {
                     <div className="works-subtitle">These are just a few of the projects I've worked on recently. Check out more on my <a target="_blank" rel="noopener" href='https://github.com/ayeyaiyai'className="works-link">github!</a></div>
                     <section className="works-grid">
                         <div className="grid-column left-column">
-                            <div className="work-grid-item grid-item-tall">
-                                <div className="grid-item-image"></div>
-                                <div className="grid-item-title"><span className="title-text">Where in the World?</span><img src={rightArrow} alt='arrow icon' className="arrow-icon"/></div>
-                            </div>
+                            <Link to='/where-in-the-world'>
+                                <div className="work-grid-item grid-item-tall">
+                                    <div className="grid-item-image" id="where-in-the-world"></div>
+                                    <div className="grid-item-title"><span className="title-text">Where in the World?</span><img src={rightArrow} alt='arrow icon' className="arrow-icon"/></div>
+                                </div>
+                            </Link>
+
                             <div className="work-grid-item grid-item-short">
-                                <div className="grid-item-image"></div>
+                                <div className="grid-item-image" id="weather-app"></div>
                                 <div className="grid-item-title"><span className="title-text">Weather App</span><img src={rightArrow} alt='arrow icon' className="arrow-icon"/></div>
                             </div>
                         </div>
                         <div className="grid-column right-column">
                             <div className="work-grid-item grid-item-short">
-                                <div className="grid-item-image"></div>
+                                <div className="grid-item-image" id="archive"></div>
                                 <div className="grid-item-title"><span className="title-text">Archive</span><img src={rightArrow} alt='arrow icon' className="arrow-icon"/></div>
                             </div>
                             <div className="work-grid-item grid-item-tall">
-                                <div className="grid-item-image"></div>
+                                <div className="grid-item-image"id="cody-portfolio"></div>
                                 <div className="grid-item-title"><span className="title-text">Cody's Portfolio</span><img src={rightArrow} alt='arrow icon' className="arrow-icon"/></div>
                             </div>
                         </div>
