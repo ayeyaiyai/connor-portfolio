bb86f9c92199e265c0f5b1e663e07527684f38e8
ayeyaiyai <105176518+ayeyaiyai@users.noreply.github.com>
2023-11-20 13:06:58 -0500

    Import SelectedWorks components

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import Works from './components/Works';
 import About from './components/About';
 import ForFun from './components/ForFun';
 import WhereInTheWorld from './components/selected-works/WhereInTheWorld';
+import WeatherApp from './components/selected-works/WeatherApp';
+import Archive from './components/selected-works/Archive';
+import CodyPortfolio from './components/selected-works/CodyPortfolio';
 
 function App() {
   return (
@@ -16,6 +19,9 @@ function App() {
         <Routes>
           <Route path='/work' element={<Works />} />
           <Route path='/where-in-the-world' element={<WhereInTheWorld />} />
+          <Route path='/weather-app' element={<WeatherApp />} />
+          <Route path='/archive' element={<Archive />} />
+          <Route path='/cody-portfolio' element={<CodyPortfolio />} />
           <Route path='/about' element={<About />} />
           <Route path='/for-fun' element={<ForFun />} />
           <Route path='/' element={<Home />} />
