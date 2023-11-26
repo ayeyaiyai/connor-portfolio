5316dd572f58060176114d6da23069eefcf21c6d
ayeyaiyai <105176518+ayeyaiyai@users.noreply.github.com>
2023-11-26 13:09:45 -0500

    Edit About text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,7 +15,7 @@ function About() {
                         <div className="about-image"><img src={headshot} alt="head shot" className="headshot"/></div>
                     </section>
                     <section className="info">
-                        <p className="info-paragraph">I'm a Columbus based self-taught front end web developer who began studying in 2022. I thoroughly enjoy problem-solving and turning complex tasks into simple, elegant solutions. Before I started my journey in web development I did data entry. I like this a lot more!</p>
+                        <p className="info-paragraph">I'm a Columbus based self-taught front end web developer who began studying in 2022. I thoroughly enjoy problem-solving and turning complex tasks into simple, elegant solutions. Before I started my journey in web development I was a barista. I like this a lot more!</p>
                         <p className="info-paragraph">I'm a life-long learner with interests ranging from analytical psychology, orangutan behavioral studies, scouring the web for obscure video games, and exercise/nutrition. I have always loved being online, so learning web development only seemed natural!</p>
                     </section>
                     <div className="section-divider"></div>
